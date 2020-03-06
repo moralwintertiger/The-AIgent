153f5928ce98f230b0b9ac9ce7ae499c40586266
Ryan Dalton <moralwintertiger@gmail.com>
2020-03-06 11:30:31 -0800

    Update metadata.py

--- similars_model/metadata.py
+++ similars_model/metadata.py
@@ -11,27 +11,27 @@ class BookData(object):
         '''Load information about a given book'''
         self.book_id = book_id
         metadata_csv = pd.read_csv(os.path.join(data_path, top_books))
-        self.record = metadata_csv[metadata_csv['source_book_id'] == book_id]
+        self.record = metadata_csv[metadata_csv['book_id'] == book_id]
         self.pull_record = lambda field: self.record[field].iloc[0]
         self.retrieve_metadata()
         self.retrieve_cover()
 
     def retrieve_metadata(self):
         '''Pull the book's metadata records'''
-        self.title = self.pull_record('source_title_desc')
-        self.year = self.pull_record('published_year')
-        self.author = self.pull_record('source_person_desc')
-        self.publisher = self.pull_record('publisher_names')
-        self.rating = self.pull_record('user_rating_avg')
-        self.rating_count = self.pull_record('person_user_rating_cnt')
-        self.book_url = self.pull_record('source_book_url')
+        self.title = self.pull_record('title')
+        self.year = self.pull_record('pub_year')
+        self.author = self.pull_record('author')
+        self.publisher = self.pull_record('pub_name')
+        self.rating = self.pull_record('user_rating_average')
+        self.rating_count = self.pull_record('user_rating_count')
+        self.book_url = self.pull_record('book_url')
         self.record_rating = 100 - int(
-            self.pull_record('user_ratings_rank') * 100)
+            self.pull_record('ratings_rank') * 100)
         self.synopsis = self.pull_record('description')
 
     def retrieve_cover(self):
         '''Pulls the book's cover art'''
-        record_url = self.pull_record('source_book_url')
+        record_url = self.pull_record('book_url')
         r = requests.get(record_url)
         html = r.text
         soup = BeautifulSoup(html, "html.parser")
